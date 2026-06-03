8c051a7d904d0cef71a5151ae904f39157201f25
kangchenghua <chenghuakang@gmail.com>
2026-06-03 09:54:08 +0200

    Update ping_to_csv.sh

--- test/handover/ping_to_csv.sh
+++ test/handover/ping_to_csv.sh
@@ -2,11 +2,17 @@
 
 TARGET="$1"
 DURATION="$2"
-INTERVAL="${3:-1}" 
-SIZE="64"          
+INTERVAL="${3:-1}"
+SIZE="64"
 
 if [ "$TARGET" = "default" ]; then
     read -r _ TARGET < "./grd_config" || exit 1
 fi
+
 COUNT="$(awk -v duration="$DURATION" -v interval="$INTERVAL" 'BEGIN { print int(duration / interval) }')"
-ping -D -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" > "ping_${TARGET}.raw" 2>&1
+
+OUTPUT_RAW="ping_${TARGET}.raw"
+
+rm -f "$OUTPUT_RAW"
+
+ping -D -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" > "$OUTPUT_RAW" 2>&1
