df8e0a453951f38d71cbe6e806c50e32f85864aa
kangchenghua <chenghuakang@gmail.com>
2026-06-03 04:34:22 +0200

    Update ping_to_csv.sh

--- test/handover/ping_to_csv.sh
+++ test/handover/ping_to_csv.sh
@@ -12,40 +12,44 @@ if [ "$TARGET" = "default" ]; then
         exit 1
     }
 fi
+
 DURATION="$2"
 INTERVAL="${3:-1}"
 SIZE="1200"
 OUTPUT="ping_${TARGET}.csv"
 RAW_OUTPUT="ping_${TARGET}.raw"
+
 COUNT="$(awk -v duration="$DURATION" -v interval="$INTERVAL" 'BEGIN { print int(duration / interval) }')"
 
-
-# Write CSV header
 echo "timestamp,icmp_seq,ttl,rtt_ms" > "$OUTPUT"
 
-# Run ping and store raw output first
-#ping -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" > "$RAW_OUTPUT"
-ping -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" | tee "$RAW_OUTPUT" | stdbuf -oL awk -v outfile="$OUTPUT" '
-/bytes from/ {
-    # Extract RTT
-    split($0,a,"time=");
-    split(a[2],b," ");
-    rtt=b[1];
-
-    # Extract sequence number
-    split($6,seq,"=");
-    icmp_seq=seq[2];
-
-    # Extract TTL
-    split($7,t,"=");
-    ttl=t[2];
-
-    # Timestamp in ISO format
-    cmd="date +%Y-%m-%dT%H:%M:%S.%3N";
-    cmd | getline timestamp;
-    close(cmd);
-
-    print timestamp "," icmp_seq "," ttl "," rtt >> outfile;
-    fflush(outfile);
+ping -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" | tee "$RAW_OUTPUT" | awk -v outfile="$OUTPUT" '
+BEGIN {
+    FS = " "
 }
-' "$RAW_OUTPUT"
+{
+    "date +%Y-%m-%dT%H:%M:%S.%3N" | getline timestamp
+    close("date +%Y-%m-%dT%H:%M:%S.%3N")
+
+    if ($0 ~ /bytes from/) {
+        split($0, a, "time=");
+        split(a[2], b, " ");
+        rtt = b[1];
+
+        icmp_seq = "0"; ttl = "0";
+        for(i=1; i<=NF; i++) {
+            if($i ~ /icmp_seq=/) { split($i, s, "="); icmp_seq = s[2]; }
+            if($i ~ /ttl=/) { split($i, t, "="); ttl = t[2]; }
+        }
+        print timestamp "," icmp_seq "," ttl "," rtt >> outfile
+    } 
+    else if ($0 ~ /icmp_seq=/) {
+        icmp_seq = "0";
+        for(i=1; i<=NF; i++) {
+            if($i ~ /icmp_seq=/) { split($i, s, "="); icmp_seq = s[2]; }
+        }
+        print timestamp "," icmp_seq ",-1,TIMEOUT" >> outfile
+    }
+    fflush(outfile)
+}
+'
