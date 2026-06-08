57bc23887cc458b2bac077ab51c41268ebe0ee2a
kangchenghua <chenghuakang@gmail.com>
2026-06-08 09:28:46 +0200

    Update ping_to_csv.sh

--- test/handover/ping_to_csv.sh
+++ test/handover/ping_to_csv.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 
-TARGET="$1"
-DURATION="$2"
-INTERVAL="${3:-1}"
-SIZE="64"
-
-if [ "$TARGET" = "default" ]; then
-    read -r _ TARGET < "./grd_config" || exit 1
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+    echo "Usage: $0 <target|default> <duration_seconds> [interval_seconds]" >&2
+    exit 1
 fi
 
+TARGET="$1"
+if [ "$TARGET" = "default" ]; then
+    read -r _ TARGET < "./grd_config" || {
+        echo "Failed to read target from ./grd_config" >&2
+        exit 1
+    }
+fi
+DURATION="$2"
+INTERVAL="${3:-0.01}"
+SIZE="1200"
+OUTPUT="ping_${TARGET}.csv"
+RAW_OUTPUT="ping_${TARGET}.raw"
 COUNT="$(awk -v duration="$DURATION" -v interval="$INTERVAL" 'BEGIN { print int(duration / interval) }')"
 
-OUTPUT_RAW="ping_${TARGET}.raw"
+# Write CSV header
+echo "timestamp,icmp_seq,ttl,rtt_ms" > "$OUTPUT"
 
-rm -f "$OUTPUT_RAW"
+# Run ping and store raw output first
+ping -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" > "$RAW_OUTPUT"
 
-ping -D -i "$INTERVAL" "$TARGET" -c "$COUNT" -s "$SIZE" > "$OUTPUT_RAW" 2>&1
+# Parse stored ping output after ping completes
+awk -v outfile="$OUTPUT" '
+/bytes from/ {
+    # Extract RTT
+    split($0,a,"time=");
+    split(a[2],b," ");
+    rtt=b[1];
+
+    # Extract sequence number
+    split($6,seq,"=");
+    icmp_seq=seq[2];
+
+    # Extract TTL
+    split($7,t,"=");
+    ttl=t[2];
+
+    # Timestamp in ISO format
+    cmd="date +%Y-%m-%dT%H:%M:%S.%3N";
+    cmd | getline timestamp;
+    close(cmd);
+
+    print timestamp "," icmp_seq "," ttl "," rtt >> outfile;
+    fflush(outfile);
+}
+' "$RAW_OUTPUT"
